fc6784b0d9ec6ad6a00301dc041ad820bf1e9e8e
Hirokazu NISHIOKA <hiro@nisshiee.org>
2012-04-20 19:53:46 +0900

    implement Board.boardJS

--- src/main/scala/org/nisshiee/puyo/core/Board.scala
+++ src/main/scala/org/nisshiee/puyo/core/Board.scala
@@ -2,6 +2,10 @@ package org.nisshiee.puyo.core
 
 import scalaz._, Scalaz._
 
+import scala.collection.JavaConversions
+
+import jp.ac.nagoya_u.is.ss.kishii.usui.system.game.{ Board => JBoard }
+
 class Board private(
   val field: Field,
   val current: PuyoBlock,
@@ -9,4 +13,23 @@ class Board private(
   val nextNext: PuyoBlock,
   val ojamaQueue: List[Int]
 ) {
+}
+
+object Board {
+  def boardJS: JBoardWrapper => Board = { j =>
+    new Board(
+      Field.fieldJS(j.field),
+      PuyoBlock.puyoBlockJS(j.currentPuyo),
+      PuyoBlock.puyoBlockJS(j.nextPuyo),
+      PuyoBlock.puyoBlockJS(j.nextNextPuyo),
+      JavaConversions.asScalaBuffer(j.ojamaList).toList ∘ (i => i: Int))
+  }
+}
+
+private[core] class JBoardWrapper(val j: JBoard) {
+  def currentPuyo = new JPuyoBlockWrapper(j.getCurrentPuyo)
+  def nextPuyo = new JPuyoBlockWrapper(j.getNextPuyo)
+  def nextNextPuyo = new JPuyoBlockWrapper(j.getNextNextPuyo)
+  def field = new JFieldWrapper(j.getField)
+  def ojamaList = j.getNumbersOfOjamaList
 }
---
+++ src/test/scala/org/nisshiee/puyo/core/BoardTest.scala
@@ -0,0 +1,61 @@
+package org.nisshiee.puyo.core
+
+import scalaz._, Scalaz._
+
+import org.specs2._, mock._
+
+import PuyoBlocks._
+
+import jp.ac.nagoya_u.is.ss.kishii.usui.system.storage.PuyoType
+
+class BoardTest extends Specification { def is =
+
+  "BoradTestクラスのテスト"                                 ^
+    "boardJSのテスト"                                       ^
+      "currentがコピーされること"                           ! JMock().e1^
+      "nextがコピーされること"                              ! JMock().e2^
+      "nextNextがコピーされること"                          ! JMock().e3^
+      "ojamaQueueがコピーされること"                        ! JMock().e4^
+                                                            end
+
+  case class JMock() extends Mockito {
+
+    val currentw = mock[JPuyoBlockWrapper]
+    currentw.base returns PuyoType.RED_PUYO
+    currentw.sub returns PuyoType.BLUE_PUYO
+
+    val nextw = mock[JPuyoBlockWrapper]
+    nextw.base returns PuyoType.GREEN_PUYO
+    nextw.sub returns PuyoType.YELLOW_PUYO
+
+    val nextNextw = mock[JPuyoBlockWrapper]
+    nextNextw.base returns PuyoType.PURPLE_PUYO
+    nextNextw.sub returns PuyoType.PURPLE_PUYO
+
+    val fw = mock[JFieldWrapper]
+    fw.deadLine returns 12
+    fw.height returns 14
+    fw.width returns 6
+    fw.puyoType(anyInt, anyInt) returns null
+
+    val olw: java.util.List[java.lang.Integer] = new java.util.ArrayList
+    olw.add(1)
+    olw.add(2)
+    olw.add(3)
+
+    val bw = mock[JBoardWrapper]
+    bw.currentPuyo returns currentw
+    bw.nextPuyo returns nextw
+    bw.nextNextPuyo returns nextNextw
+    bw.field returns fw
+    bw.ojamaList returns olw
+
+    def e1 = Board.boardJS(bw).current ≟ PuyoBlock(Red, Blue) must beTrue
+
+    def e2 = Board.boardJS(bw).next ≟ PuyoBlock(Green, Yellow) must beTrue
+
+    def e3 = Board.boardJS(bw).nextNext ≟ PuyoBlock(Purple, Purple) must beTrue
+
+    def e4 = Board.boardJS(bw).ojamaQueue ≟ List(1, 2, 3) must beTrue
+  }
+}
